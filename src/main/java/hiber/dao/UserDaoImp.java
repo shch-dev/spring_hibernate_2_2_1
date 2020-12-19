65ca2b95317daddf141a7748c0adce5132dfa550
shch-dev <ivan.poleshchuk.russia@gmail.com>
2020-12-19 11:03:24 +0300

    Task 2.2.1

---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,39 @@
+package hiber.dao;
+
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    public User getUserCarModelAndSeries(String model, int series) {
+        Query<User> query = sessionFactory.getCurrentSession()
+                .createQuery("from User where car.series =: series and car.model =: model")
+                .setParameter("series", series)
+                .setParameter("model", model);
+        return query.getSingleResult();
+    }
+}
